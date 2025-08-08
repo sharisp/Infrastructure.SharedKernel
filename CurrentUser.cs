25cfc1eaaa0ce682ce08dce54052acee0b50a245
Andrew Wang <wthsir@gmail.com>
2025-08-08 16:57:31 +1000

    refactor: add key comments

--- CurrentUser.cs
+++ CurrentUser.cs
@@ -4,6 +4,10 @@ using System.Security.Claims;
 
 namespace Infrastructure.SharedKernel
 {
+    /// <summary>
+    /// CurrentUser, to get the current user information from the HTTP context.
+    /// </summary>
+    /// <param name="contextAccessor"></param>
     public class CurrentUser(IHttpContextAccessor contextAccessor) : ICurrentUser
     {
         public long? UserId =>
